890dd95c6e99ee25544522d6c796f44aff2c5a0a
rickos <rick@rosullivan.net>
2021-06-01 22:46:18 +1000

    fix GraphQL listWhiteboards query

--- src/store/whiteboards/actions.js
+++ src/store/whiteboards/actions.js
@@ -2,11 +2,22 @@ import API, { graphqlOperation } from "@aws-amplify/api";
 import { listWhiteboaardss } from "src/graphql/queries";
 
 export async function fetchWhiteboards(){
-    return new Promise(async (resolve, rreject)=> {
+    return new Promise(async (resolve, reject)=> {
         try {
-            const resultdata = await API.graphql(graphqlOperation(listWhiteboaardss))
-            console.log("Hello Rick")
-            const whiteboards = resultdata.map(whiteboard => new Whiteboard(Whiteboard))
+            const {
+                // @ts-ignore
+                data: {
+                    listWhiteboaardss: { items: whiteboardData }
+                    }
+                } = await API.graphql(graphqlOperation(listWhiteboaardss))
+          
+              // data mutations happen within a Flight class
+              // here we convert graphQL results into an array of Flights
+              // before committing to Vuex State Management
+              // const whiteboards = whiteboardData.map((whiteboard) => new Whiteboard(whiteboard))
+
+                console.log("Hello Rick", whiteboardData)
+                // const whiteboards = resultdata.map(whiteboard => new Whiteboard(Whiteboard))
         } catch (error){
             console.error(error)
             }
